b50bda9d2a6e6e81879beb633b17da16edbab020
Thomas Heller <thomas.heller@cs.fau.de>
2018-08-07 21:28:47 +0200

    Adding remaining testcases to run script

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import subprocess, os, sys, socket
+import multiprocessing
 
 if len(sys.argv) >= 2:
-    run=sys.argv[1:]
+    max_cores = int(sys.argv[1])
+    run=sys.argv[2:]
 else:
+    max_cores = multiprocessing.cpu_count()
     run='all'
 
 my_env = os.environ.copy()
@@ -35,7 +38,6 @@ if 'tasks' in run or run == 'all':
         p.wait()
 
 if 'scheduling' in run or run == 'all':
-    import multiprocessing
 
     benchmarks=[
         'scheduling/hpx_scheduling',
@@ -45,7 +47,7 @@ if 'scheduling' in run or run == 'all':
         'scheduling/tbb_scheduling',
     ]
 
-    for threads in range(1, multiprocessing.cpu_count() + 1):
+    for threads in range(1, max_cores + 1):
 
         my_env['NUM_THREADS'] = str(threads)
 
@@ -76,6 +78,74 @@ if 'serialization' in run or run == 'all':
             os.makedirs(os.path.dirname(result))
         bench = [
                 benchmark + ' --hpx:threads=1 --benchmark_out_format=json --benchmark_out=' + result]
+        p = subprocess.Popen(bench, env = my_env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(p.stdout.read())
+        p.wait()
+
+if 'distributed' in run or run == 'all':
+    benchmarks=[
+        'distributed/async_latency',
+        'distributed/channel_send_recv',
+        'distributed/components',
+    ]
+    my_env['NUM_THREADS'] = '%s' % max_cores
+    my_env['OMP_NUM_THREADS'] = '1'
+    for benchmark in benchmarks:
+        print('  %s' % benchmark)
+        result = os.path.join(result_dir, benchmark + '.json')
+        if not os.path.exists(os.path.dirname(result)):
+            os.makedirs(os.path.dirname(result))
+        bench = [
+                'srun ' + benchmark + (' --hpx:threads=%s --benchmark_out_format=json --benchmark_out=' % max_cores) + result]
+        p = subprocess.Popen(bench, env = my_env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(p.stdout.read())
+        p.wait()
+
+    benchmarks=[
+        'distributed/mpi_latency',
+    ]
+    my_env['NUM_THREADS'] = '%s' % max_cores
+    my_env['OMP_NUM_THREADS'] = '1'
+    for benchmark in benchmarks:
+        print('  %s' % benchmark)
+        result = os.path.join(result_dir, benchmark + '.json')
+        if not os.path.exists(os.path.dirname(result)):
+            os.makedirs(os.path.dirname(result))
+        bench = [
+                'srun ' + benchmark + ' --benchmark_out_format=json --benchmark_out=' + result]
+        p = subprocess.Popen(bench, env = my_env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(p.stdout.read())
+        p.wait()
+
+if 'broadcast' in run or run == 'all':
+    benchmarks=[
+        'distributed/broadcast',
+    ]
+    my_env['NUM_THREADS'] = '%s' % max_cores
+    my_env['OMP_NUM_THREADS'] = '1'
+    for benchmark in benchmarks:
+        print('  %s' % benchmark)
+        result = os.path.join(result_dir, benchmark + '.json')
+        if not os.path.exists(os.path.dirname(result)):
+            os.makedirs(os.path.dirname(result))
+        bench = [
+                'srun ' + benchmark + (' --hpx:threads=%s --benchmark_out_format=json --benchmark_out=' % max_cores) + result]
+        p = subprocess.Popen(bench, env = my_env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(p.stdout.read())
+        p.wait()
+
+    benchmarks=[
+        'distributed/mpi_broadcast',
+    ]
+    my_env['NUM_THREADS'] = '%s' % max_cores
+    my_env['OMP_NUM_THREADS'] = '1'
+    for benchmark in benchmarks:
+        print('  %s' % benchmark)
+        result = os.path.join(result_dir, benchmark + '.json')
+        if not os.path.exists(os.path.dirname(result)):
+            os.makedirs(os.path.dirname(result))
+        bench = [
+                'srun ' + benchmark + ' --benchmark_out_format=json --benchmark_out=' + result]
         p = subprocess.Popen(bench, env = my_env, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(p.stdout.read())
         p.wait()
